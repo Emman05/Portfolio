0f29a6c6eda1646c33c561b7f33ae05902366b36
emman05 <emmancool05@gmail.com>
2023-08-09 21:25:11 +0530

    banner and about page updated

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,27 @@
 import React from 'react';
+// countup
+import CountUp from 'react-countup';
+// intersection observer hook
+import { useInView } from 'react-intersection-observer';
+//motion
+import { motion } from 'framer-motion';
+//variant
+import { fadeIn } from '../variants';
 
 const About = () => {
-  return <div className='section' id='about'>
-    About
-    </div>;
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
+  return ( 
+  <section className='section' id='about' ref={ref}>
+    <div className='container mx-auto'>
+      <div>
+        {/* img */}
+        <div></div>
+      </div>
+    </div>
+    </section>
+  );
 };
 
 export default About;
